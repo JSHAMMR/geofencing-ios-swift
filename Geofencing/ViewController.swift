250e294d06e204b551d138a13366f87b4d79d7a1
Jamil Shamar <gamilaliqaid.shamar@xchanging.com>
2019-11-03 00:30:37 +0800

    init commit

---
+++ Geofencing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Geofencing
+//
+//  Created by Gamil Ali Qaid Shamar on 03/11/2019.
+//  Copyright © 2019 com.xchanging.agencydaiichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
